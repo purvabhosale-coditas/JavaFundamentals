f04e2ac01b6b1fcdd83198295d1b2ded28199604
Purva Bhosale <purva.bhosale@coditas.com>
2026-02-02 21:32:55 +0530

    Initial commit - JavaAssignment1 project

---
+++ JavaAssignment1/src/javacoursepackage/Product.java
@@ -0,0 +1,22 @@
+package javacoursepackage;
+
+public class Product {
+	
+	int id, available_quantity;
+	String name;
+	double price, gst;
+	
+	  Product(int id, int available_quantity, String name, double price, double gst) {
+        this.id = id;
+        this.available_quantity = available_quantity;
+        this.name = name;
+        this.price = price;
+        this.gst = gst;        
+	  }
+	  
+	  double priceWithGST() {
+          return price + (price * gst / 100);
+      }
+}
+
+
